087e2ce30d347846d2fb70561d028790a15b50cc
Julian Rojas <julianrojasbet@gmail.com>
2020-11-24 15:53:07 -0500

    Add Oferta services

--- hv_estudiantes_back/core/views.py
+++ hv_estudiantes_back/core/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from rest_framework import viewsets
+from .models import Oferta
+from .serializers import OfertaSerializer
 
 from django.http import HttpResponse
 
@@ -34,3 +37,8 @@ def signupOffer(request):
 def addNewOffer(request):
     form = AddNewOfferForm()
     return render(request,'offer/addNewOffer.html', {'form': form})
+
+
+class OfertaViewset(viewsets.ModelViewSet):
+    serializer_class = OfertaSerializer
+    queryset = Oferta.objects.all()
